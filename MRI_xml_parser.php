3e81ed92d2bf14c2fc7ccff0343bebd9fde2ccd1
med-men <med-men@gmx.net>
2020-09-17 23:49:41 +0200

    first ideas of xml parsing, THIS DOES NOT WORK YET

---
+++ MRI_xml_parser.php
@@ -0,0 +1,65 @@
+<?php
+$countIx = 0;
+
+$xml = new XMLReader();
+$xml->open('./uploads/Oberbauch.xml');
+
+while($xml->read() && $xml->name != 'SubStep')
+{
+    ;
+}
+
+while($xml->name == 'SubStep')
+{
+    $element = new SimpleXMLElement($xml->readOuterXML()); //
+
+    $prod = array(
+        'name' => strval($element->ProtHeaderInfo->HeaderProtPath),
+        'TA' => strval($element->ProtHeaderInfo->HeaderProperty),
+    );
+
+    $target_elements = array('Schichten', 'Phasenkod.-Richt.', 'FoV Auslese', 'TR', 'TE');
+
+    foreach ($element->Card->ProdParameter as $seq_property) {
+        if(in_array(strval($seq_property->label), $target_elements)){
+            $label = strval($seq_property->label);
+            $value = strval($seq_property->ValueAndUnit);
+            $prod[$label] = $value;
+        }
+    }
+
+
+    print_r($prod);
+    print "\n";
+    $countIx++;
+
+    $xml->next('ProtHeaderInfo');
+    unset($element);
+}
+
+print "Number of items=$countIx\n";
+print "memory_get_usage() =" . memory_get_usage()/1024 . "kb\n";
+print "memory_get_usage(true) =" . memory_get_usage(true)/1024 . "kb\n";
+print "memory_get_peak_usage() =" . memory_get_peak_usage()/1024 . "kb\n";
+print "memory_get_peak_usage(true) =" . memory_get_peak_usage(true)/1024 . "kb\n";
+
+print "custom memory_get_process_usage() =" . memory_get_process_usage() . "kb\n";
+
+
+$xml->close();
+
+/**
+* Returns memory usage from /proc<PID>/status in bytes.
+*
+* @return int|bool sum of VmRSS and VmSwap in bytes. On error returns false.
+*/
+function memory_get_process_usage() {
+    $status = file_get_contents('/proc/' . getmypid() . '/status');
+    $matchArr = array();
+    preg_match_all('~^(VmRSS|VmSwap):\s*([0-9]+).*$~im', $status, $matchArr);
+    if(!isset($matchArr[2][0]) || !isset($matchArr[2][1])) {
+        return false;
+    }
+
+    return intval($matchArr[2][0]) + intval($matchArr[2][1]);
+}
